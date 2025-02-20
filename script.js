e46acf3dda9e01c62edd97973ed60fb7e8732098
SamuelFreitasSoares <samuelfreitassoares@outlook.com>
2025-02-20 16:09:13 -0300

    :sparkles: feat: added function that simulates the computer choosing [rock]-[paper]-[scissors] using Math.random

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let x = Math.random()
+
+    if (x < 0.333){
+        return "Rock!";
+    } else if (x > 0.333 && x < 0.666){
+        return "Paper!";
+    } else {
+        return "Scissors!";
+    }
+    
+}
+
